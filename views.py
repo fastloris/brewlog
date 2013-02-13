fb7409f147cd2ac284165ec8819cd8317c660a7f
jason <jason@vontrance.com>
2013-02-13 11:04:34 -0800

    rename brew detail pages in prep for adding recipes

--- views.py
+++ views.py
@@ -7,8 +7,8 @@ from django.http import Http404
 def index(request):
     latest_brewday_list = BrewDay.objects.order_by('-date')[:5]
     context = {'latest_brewday_list': latest_brewday_list}
-    return render(request, 'brewlog/index.html', context)
+    return render(request, 'brewlog/brewindex.html', context)
 
 def detail(request, brewday_id):
     brewday = get_object_or_404(BrewDay, pk=brewday_id)
-    return render(request, 'brewlog/detail.html', {'brewday': brewday})
+    return render(request, 'brewlog/brewdetail.html', {'brewday': brewday})
